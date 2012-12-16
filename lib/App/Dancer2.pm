f748b43fa9d344109a72f229f39cd726667ee040
celogeek <me@celogeek.com>
2012-12-16 15:37:17 +0100

    try cloning repos

--- lib/App/Dancer2.pm
+++ lib/App/Dancer2.pm
@@ -4,6 +4,8 @@ use warnings;
 use Moo;
 use MooX::Options;
 use feature 'say';
+use Git::Raw;
+use File::Spec;
 
 option 'app' => (
     is => 'ro',
@@ -17,6 +19,20 @@ option 'app' => (
 sub create_app {
     my $self = shift;
     say "Creating app : ", $self->app;
+
+    my $url = 'git://gitorious.celogeek.com/perl-dancer2/basic.git';
+    my $path = File::Spec->catfile($ENV{PWD}, $self->app);
+    my $repo = Git::Raw::Repository->init($path, 0);
+    my $remote = Git::Raw::Remote->add($repo, 'origin', $url);
+    $remote->connect('fetch');
+    $remote->download;
+    $remote->update_tips;
+    $remote->disconnect;
+    $repo->checkout($repo->head, {
+            'update_missing'  => 1,
+            'update_modified' => 1
+        });
+    return;
 }
 
 1;
